38169b4ec9c240dd3fd87635a2309cbe35a8db0c
ChiefLopes <chieflopesekede@gmail.com>
2023-09-14 12:02:10 +0100

    added movies to the movie secrtion from the api

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -1,22 +1,34 @@
-import { useEffect} from 'react'
+import { useEffect, useState } from "react";
 
 const MovieSection = () => {
-        const getMovies = () => {
-          fetch(
-            "https://api.themoviedb.org/3/discover/movie?api_key=52fc64fc69ad74d43acbf242d5f72966"
-          )
-            .then((response) => response.json())
-            .then((response) => console.log(response));
-        };
+  const [listMovies, setListMovies] = useState([]);
+  const getMovies = () => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=52fc64fc69ad74d43acbf242d5f72966"
+    )
+      .then((response) => response.json())
+      .then((json) => setListMovies(json.results));
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
     
-    useEffect(() => {
-        getMovies()
-    },[])
-        
-      return <div></div>;
-    }
-    
+    console.log(listMovies);
 
+    return (
+      <>
+        <div className="">
+          {listMovies.map((movie) => (
+            <img
+              key={movie.id} 
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt="movie posters"
+            />
+          ))}
+        </div>
+      </>
+    );
+};
 
-
-export default MovieSection
+export default MovieSection;
