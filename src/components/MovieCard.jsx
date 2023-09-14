fe297df24ff7c3293aa832e7ce1caf67220968cf
ChiefLopes <chieflopesekede@gmail.com>
2023-09-14 15:22:22 +0100

    added the genre_ids to the movie card using a function the returns the genre names in place of the numbers

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,96 @@
 import PropTypes from "prop-types";
 
 const MovieCard = ({ movie }) => {
+   const genresData = {
+     genres: [
+       {
+         id: 28,
+         name: "Action",
+       },
+       {
+         id: 12,
+         name: "Adventure",
+       },
+       {
+         id: 16,
+         name: "Animation",
+       },
+       {
+         id: 35,
+         name: "Comedy",
+       },
+       {
+         id: 80,
+         name: "Crime",
+       },
+       {
+         id: 99,
+         name: "Documentary",
+       },
+       {
+         id: 18,
+         name: "Drama",
+       },
+       {
+         id: 10751,
+         name: "Family",
+       },
+       {
+         id: 14,
+         name: "Fantasy",
+       },
+       {
+         id: 36,
+         name: "History",
+       },
+       {
+         id: 27,
+         name: "Horror",
+       },
+       {
+         id: 10402,
+         name: "Music",
+       },
+       {
+         id: 9648,
+         name: "Mystery",
+       },
+       {
+         id: 10749,
+         name: "Romance",
+       },
+       {
+         id: 878,
+         name: "Science Fiction",
+       },
+       {
+         id: 10770,
+         name: "TV Movie",
+       },
+       {
+         id: 53,
+         name: "Thriller",
+       },
+       {
+         id: 10752,
+         name: "War",
+       },
+       {
+         id: 37,
+         name: "Western",
+       },
+     ],
+   };
+    
+    function getGenreNamesByIds(genreIds) {
+  const genres = genresData.genres;
+  return genreIds.map((genreId) => {
+    const genre = genres.find((g) => g.id === genreId);
+    return genre ? genre.name : "Unknown"; // Return the genre name or "Unknown" if not found
+  });
+}
+    
+
   return (
     <div data-testid="movie-card" className="bg-white rounded-lg shadow-md p-4">
       <img
@@ -10,13 +100,16 @@ const MovieCard = ({ movie }) => {
         alt={movie.title}
         className="w-full rounded-lg mb-2"
       />
-      <h2 data-testid="movie-title" className="text-xl font-semibold mb-1">
-        {movie.title}
-      </h2>
+
+      <p className="text-zinc-950 font-semibold mb-3 text-xl">{movie.title}</p>
       <p data-testid="movie-release-date" className="text-gray-600 mb-2">
-        Release Date: {movie.genre_ids}
+        {`${movie.vote_average * 10}.0`}/100
       </p>
-      <p className="text-gray-700">{movie.genre_ids}</p>
+      <h2
+        data-testid="genre_ids"
+        className=" font-medium text-gray-600  mb-1 ">
+        {getGenreNamesByIds(movie.genre_ids).join(", ")}
+      </h2>
     </div>
   );
 };
@@ -24,7 +117,7 @@ const MovieCard = ({ movie }) => {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    release_date: PropTypes.string.isRequired,
+    vote_average: PropTypes.string.isRequired,
     poster_path: PropTypes.string.isRequired,
     genre_ids: PropTypes.string.isRequired,
   }).isRequired,
