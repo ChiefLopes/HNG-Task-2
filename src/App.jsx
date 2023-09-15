3aa11fb3739bf258d98f1eb6becf738ada61e28a
ChiefLopes <chieflopesekede@gmail.com>
2023-09-15 14:21:09 +0100

    removed the movie detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Hero from "./components/Hero.jsx";
-import MovieDetailPage from "./components/MovieDetailPage";
 import MovieSection from "./components/MovieSection";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <Hero />
       <MovieSection />
-      <MovieDetailPage />
     </div>
   );
 }
